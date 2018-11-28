371a7b2b4090834858e8f59b00784c6ecdea6360
Sandared <thomas.driessen.td@gmail.com>
2018-11-28 16:16:49 +0000

    Benchmark is working now, added comments

--- benchmarks/src/main/java/io/jatoms/jmh/OSGiBenchmark2.java
+++ benchmarks/src/main/java/io/jatoms/jmh/OSGiBenchmark2.java
@@ -21,12 +21,9 @@ import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Threads;
 import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.infra.Blackhole;
-import org.osgi.framework.AllServiceListener;
 import org.osgi.framework.Bundle;
-import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleException;
 import org.osgi.framework.InvalidSyntaxException;
-import org.osgi.framework.ServiceEvent;
 import org.osgi.framework.ServiceReference;
 import org.osgi.service.component.ComponentFactory;
 import org.osgi.service.component.ComponentInstance;
@@ -40,20 +37,19 @@ import de.unia.smds.test.ITest;
 @Measurement(iterations = 5, time = 10, timeUnit = TimeUnit.SECONDS)
 public class OSGiBenchmark2 {
 
-    Felix felix;
     ComponentFactory<ITest> factoryService;
-
     Dictionary<String, Object> configSmall;
     Dictionary<String, Object> configLarge;
 
     @Setup(Level.Trial)
     public void prepare() throws BundleException, FileNotFoundException, InterruptedException, InvalidSyntaxException {
+        // Configure Felix to use the framework classloader and delegate the classloading for org.osgi.service.component to this one,
+        // as otherwise we get nasty classcastexceptions, as the framework and the bootstrapping code would have different instances of the same class
         Map<String, Object> config = new HashMap<>();
-        // config.put("org.osgi.framework.system.packages.extra", "org.osgi.service.component");
         config.put("org.osgi.framework.bootdelegation", "org.osgi.service.component");
         config.put("org.osgi.framework.bundle.parent", "framework");
 
-        felix = new Felix(config);
+        Felix felix = new Felix(config);
         felix.start();
 
         // install bundles for the test
@@ -63,20 +59,16 @@ public class OSGiBenchmark2 {
         Path testlib = Paths.get(
                 "/workspace/jmh-benchmarks/benchmarks/libs/io/jatoms/test/test.bundle/0.1.0/test.bundle-0.1.0.jar");
 
-        Bundle scrBundle = felix.getBundleContext().installBundle("scr", new FileInputStream(scr.toFile()));
-        // scrBundle.getBundleContext().addServiceListener(new MyServiceListener(scrBundle.getBundleContext()));
-
-        Bundle testlibBundle = felix.getBundleContext().installBundle("testlib", new FileInputStream(testlib.toFile()));
+        felix.getBundleContext().installBundle("scr", new FileInputStream(scr.toFile())).start();;
+        felix.getBundleContext().installBundle("testlib", new FileInputStream(testlib.toFile())).start();
 
         System.out.println("Felix Bundle Context: " + felix.getBundleContext());
 
-        scrBundle.start();
-        testlibBundle.start();
-
+        // wait for osgi to start all services we need
         Thread.sleep(1000);
 
+        // the framework bundle has no service reference to ComponentFactory so we search the other bundles for such a reference
         Bundle[] bundles = felix.getBundleContext().getBundles();
-
         for(Bundle bundle : bundles){
             ServiceReference<ComponentFactory> ref = bundle.getBundleContext().getServiceReference(ComponentFactory.class);
             System.out.println("Reference: " + ref);
